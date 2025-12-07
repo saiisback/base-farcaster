bdef1c0b5230760c35c21f4bf496cb09ee141ca5
saiisback <karthiksaiketha@gmail.com>
2025-12-07 19:17:55 +0530

    fixed the build the errors-8

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,27 +3,12 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
-    // TODO: Add your own account association
-    frame: {
-      version: "1",
-      name: "Baseneko",
-      iconUrl: `https://pur-base.vercel.app/images/default.png`,
-      homeUrl: `https://pur-base.vercel.app`,
-      imageUrl: `https://pur-base.vercel.app/images/default.png`,
-      screenshotUrls: [],
-      tags: ["baseneko", "farcaster", "miniapp", "template"],
-      primaryCategory: "education",
-      buttonTitle: "Launch Baseneko",
-      splashImageUrl: `https://pur-base.vercel.app/images/default.png`,
-      splashBackgroundColor: "#ffffff",
-      webhookUrl: `https://pur-base.vercel.app/api/webhook`,
-      "accountAssociation": {
-        "header": "eyJmaWQiOjE1NzA1NTMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhBMEQ4ZDhiNzgyNWQwMDViMmQ1NmU2NDdkZkY1MDVBODZERjQ1ZDVCIn0",
-        "payload": "eyJkb21haW4iOiJwdXItYmFzZS52ZXJjZWwuYXBwIn0",
-        "signature": "rs8rx/Rnu/UsKEIMsmRLU4P8WU8wFQzx7Ti1451XUxlKoiUcAcx0oWi6oNbpjEJAktiUtB/CMeN4WjzKWEYchBw="
-      }
-    },
-  };
+    "accountAssociation": {
+      "header": "eyJmaWQiOjE1NzA1NTMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhBMEQ4ZDhiNzgyNWQwMDViMmQ1NmU2NDdkZkY1MDVBODZERjQ1ZDVCIn0",
+      "payload": "eyJkb21haW4iOiJwdXItYmFzZS52ZXJjZWwuYXBwIn0",
+      "signature": "rs8rx/Rnu/UsKEIMsmRLU4P8WU8wFQzx7Ti1451XUxlKoiUcAcx0oWi6oNbpjEJAktiUtB/CMeN4WjzKWEYchBw="
+    }
+  }
 
   return NextResponse.json(farcasterConfig);
 }
