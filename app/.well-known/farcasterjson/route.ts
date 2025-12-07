418b82906438599cf65c6009ca2e8cfee1ad09fb
saiisback <karthiksaiketha@gmail.com>
2025-12-07 19:13:40 +0530

    fixed the build the errors-6

--- app/.well-known/farcasterjson/route.ts
+++ app/.well-known/farcasterjson/route.ts
@@ -6,24 +6,26 @@ export async function GET() {
     // TODO: Add your own account association
     frame: {
       version: "1",
-      name: "Monad Farcaster MiniApp Template",
-      iconUrl: `${APP_URL}/images/icon.png`,
-      homeUrl: `${APP_URL}`,
-      imageUrl: `${APP_URL}/images/feed.png`,
+      name: "Baseneko",
+      iconUrl: `https://pur-base.vercel.app/images/default.png`,
+      homeUrl: `https://pur-base.vercel.app`,
+      imageUrl: `https://pur-base.vercel.app/images/default.png`,
       screenshotUrls: [],
-      tags: ["monad", "farcaster", "miniapp", "template"],
-      primaryCategory: "developer-tools",
-      buttonTitle: "Launch Template",
-      splashImageUrl: `${APP_URL}/images/splash.png`,
+      tags: ["baseneko", "farcaster", "miniapp", "template"],
+      primaryCategory: "education",
+      buttonTitle: "Launch Baseneko",
+      splashImageUrl: `https://pur-base.vercel.app/images/default.png`,
       splashBackgroundColor: "#ffffff",
-      webhookUrl: `${APP_URL}/api/webhook`,
-        accountAssociation: {
-          "header": "eyJmaWQiOjE1NzA1NTMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhBMEQ4ZDhiNzgyNWQwMDViMmQ1NmU2NDdkZkY1MDVBODZERjQ1ZDVCIn0",
-          "payload": "eyJkb21haW4iOiJwdXItYmFzZS52ZXJjZWwuYXBwIn0",
-          "signature": "rs8rx/Rnu/UsKEIMsmRLU4P8WU8wFQzx7Ti1451XUxlKoiUcAcx0oWi6oNbpjEJAktiUtB/CMeN4WjzKWEYchBw="
-        }
+      webhookUrl: `https://pur-base.vercel.app/api/webhook`,
+      "accountAssociation": {
+        "header": "eyJmaWQiOjE1NzA1NTMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhBMEQ4ZDhiNzgyNWQwMDViMmQ1NmU2NDdkZkY1MDVBODZERjQ1ZDVCIn0",
+        "payload": "eyJkb21haW4iOiJwdXItYmFzZS52ZXJjZWwuYXBwIn0",
+        "signature": "rs8rx/Rnu/UsKEIMsmRLU4P8WU8wFQzx7Ti1451XUxlKoiUcAcx0oWi6oNbpjEJAktiUtB/CMeN4WjzKWEYchBw="
+      }
     },
   };
 
   return NextResponse.json(farcasterConfig);
 }
+
+
