c9d9e2138b452ec08c96c0b48540368e7ff4af7b
saiisback <karthiksaiketha@gmail.com>
2025-12-07 18:50:16 +0530

    fixed the build the errors-2

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -17,6 +17,12 @@ export async function GET() {
       splashImageUrl: `${APP_URL}/images/splash.png`,
       splashBackgroundColor: "#ffffff",
       webhookUrl: `${APP_URL}/api/webhook`,
+      accountAssociation: {
+        "accountAssociation": {
+          "header": "eyJmaWQiOjE1NzA1NTMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhBMEQ4ZDhiNzgyNWQwMDViMmQ1NmU2NDdkZkY1MDVBODZERjQ1ZDVCIn0",
+          "payload": "eyJkb21haW4iOiJwdXItYmFzZS52ZXJjZWwuYXBwIn0",
+          "signature": "rs8rx/Rnu/UsKEIMsmRLU4P8WU8wFQzx7Ti1451XUxlKoiUcAcx0oWi6oNbpjEJAktiUtB/CMeN4WjzKWEYchBw="
+        }}
     },
   };
 
