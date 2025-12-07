d992a0d3486d1f0a42c0ec25ae2fbde9f59ccde5
saiisback <karthiksaiketha@gmail.com>
2025-12-07 19:29:20 +0530

    Add Farcaster configuration details to route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -3,6 +3,19 @@ import { APP_URL } from "../../../lib/constants";
 
 export async function GET() {
   const farcasterConfig = {
+    "frame": {
+    "name": "baseneko",
+    "version": "1",
+    "iconUrl": "https://pur-base.vercel.app/default.png",
+    "homeUrl": "https://pur-base.vercel.app",
+    "imageUrl": "https://pur-base.vercel.app/default.png",
+    "splashImageUrl": "https://pur-base.vercel.app/default.png",
+    "splashBackgroundColor": "#000000",
+    "webhookUrl": "https://pur-base.vercel.app/api/webhook",
+    "subtitle": "cat that gets you everything",
+    "description": "have fun with cat",
+    "primaryCategory": "education"
+  },
     "accountAssociation": {
       "header": "eyJmaWQiOjE1NzA1NTMsInR5cGUiOiJhdXRoIiwia2V5IjoiMHhBMEQ4ZDhiNzgyNWQwMDViMmQ1NmU2NDdkZkY1MDVBODZERjQ1ZDVCIn0",
       "payload": "eyJkb21haW4iOiJwdXItYmFzZS52ZXJjZWwuYXBwIn0",
